f0bcc1ed0c56bc6dcefb2fc86f423ec807bf91cb
andrejajanja <andrejajanja@gmail.com>
2022-05-27 14:33:30 +0200

    dodat fajl koji se sadrzati template za sve sql komande

---
+++ sql_komande.sql
@@ -0,0 +1,10 @@
+CREATE TABLE Korisnici(
+    id_korisnika int NOT NULL PRIMARY KEY,
+    ime varchar(255),
+    pass varchar(255),
+    lokacija_foldera varchar(511),
+    boja varchar(30)
+);
+
+INSERT INTO Korisnici
+VALUES ()
